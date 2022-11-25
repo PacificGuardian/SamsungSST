2bdbf32c545eb6ea6f7f57f95350f6a9dd6a1485
Pacarius <112364954+Pacarius@users.noreply.github.com>
2022-11-25 13:41:25 +0800

    AAA

--- SAMSUNG/Assets/SAMSUNG/Scripts/Buttonlist.cs
+++ SAMSUNG/Assets/SAMSUNG/Scripts/Buttonlist.cs
@@ -11,24 +11,7 @@ public class Buttonlist : MonoBehaviour {
     private void Start() {
         varManager = VarManager.Singleton;
     }
-    /*
-    List of actions:
-    >"How can I help you?"
-    
-    >" This shit was made in China."
 
-    >Add pocky to item list
-    
-    >"Cum in your ass is currently on sale! Would you like one?"
-
-    >Add cream up his ass;
-
-    >"Our big fat giant cocks are on a buy 2 get one free sale! Would you like one up your ass?"
-
-    >Add cocks
-
-    >Exit
-    */
     private void Update() {
         if(Input.anyKeyDown && Idle.slaveReady && stage < 9){
             switch(stage){
